ef980f75ce02e46f3e7f6dfd79f85b0b4330b1e1
MrSoubi <122730595+MrSoubi@users.noreply.github.com>
2023-10-31 09:16:00 +0100

    Initial commit

---
+++ GlobalHeader.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GLOBAL_HEADER
+#define GLOBAL_HEADER
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+#endif // !"GLOBAL_HEADER"
